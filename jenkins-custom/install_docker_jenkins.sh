882205f9976c1460157e2f5046c5d1407a43062c
Romain REVEL <rrr.revel@hotmail.fr>
2022-10-10 21:59:50 +0200

    MaJ script avec plugins Jenkins

--- jenkins-custom/install_docker_jenkins.sh
+++ jenkins-custom/install_docker_jenkins.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# Script contenant les commandes du didacticiel:
+# https://www.digitalocean.com/community/tutorials/how-to-automate-jenkins-setup-with-docker-and-jenkins-configuration-as-code
 
+# Partie 1
+# https://www.digitalocean.com/community/tutorials/how-to-automate-jenkins-setup-with-docker-and-jenkins-configuration-as-code#step-1-disabling-the-setup-wizard
+# 
 PROJECT_NAME="ajc-projet-final-2";
 
 git clone "https://github.com/Romain-Revel/${PROJECT_NAME}"
@@ -8,6 +13,17 @@ cd "${PROJECT_NAME}"
 git checkout jenkins
 
 cd "jenkins-custom"
+
+docker build -t jenkins:jcasc .
+
+docker run --name jenkins --rm -p 8080:8080 jenkins:jcasc
+
+# CTRL+C
+
+# Partie 2
+# https://www.digitalocean.com/community/tutorials/how-to-automate-jenkins-setup-with-docker-and-jenkins-configuration-as-code#step-2-installing-jenkins-plugins
+# 
+git pull
 
 docker build -t jenkins:jcasc .
 
