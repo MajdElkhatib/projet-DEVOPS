e2ada20b7fb7cf1ba08d9c1760c1edda234f2d5f
Romain REVEL <rrr.revel@hotmail.fr>
2022-10-11 16:53:36 +0200

    Correction jenkins-custom

--- jenkins-custom/install_docker.sh
+++ jenkins-custom/install_docker.sh
@@ -2,7 +2,7 @@
 sudo yum -y update
 
 # install docker
-sudo yum install -y git
+sudo yum install -y git java-11-openjdk
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo usermod -aG docker vagrant
--- jenkins-custom/install_docker_jenkins.sh
+++ jenkins-custom/install_docker_jenkins.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 # Script final
+newgrp docker
 
 PROJECT_NAME="ajc-projet-final-2";
 
 git clone "https://github.com/Romain-Revel/${PROJECT_NAME}";
-
+pwd
 cd "${PROJECT_NAME}";
+pwd
 
+ls -la
 git checkout dev
+ls -la
 
+pwd
 cd "jenkins-custom"
+pwd
+ls -la
 
 docker build -t jenkins:jcasc .
 docker run --name jenkins -dit -p 8080:8080 --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=password jenkins:jcasc
 
 # Import des jobs
-yum install -y java
 bash jenkins-import-jobs.sh
 
 # Ngrok
